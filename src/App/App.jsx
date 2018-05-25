a7ce1e084807909ce365029c3ef2861b9666ccc9
Chernyshov <xpom55@mail.ru>
2018-05-25 15:26:55 +0300

    init commit

---
+++ src/App/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const name = 'Josh Perez';
+
+const element = <h1>Hello, {name}</h1>;
+
+export default element;
+
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: { main: './src/main.jsx' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
