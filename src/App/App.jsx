62ea5641626488f9ec486c4187f6026c8a57d7fa
Chernyshov <xpom55@mail.ru>
2018-05-25 15:59:52 +0300

    connected socket.io-client to react

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client'
 
-const name = 'Josh Perez';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-const element = <h1>Hello, {name}</h1>;
+    this.onClick = this.onClick.bind(this);
 
-export default element;
+    this.state = {
+      endpoint: "http://localhost:4001"
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello App HELLO NIKITA уууууууу</h1>
+        <button onClick={this.onClick}>Click me!</button>
+      </div>
+    );
+  }
+
+  onClick() {
+    console.log('click');
+
+    const socket = socketIOClient(this.state.endpoint)
+
+    // this emits an event to the socket (your server) with an argument of 'red'
+    // you can make the argument any color you would like, or any kind of data you want to send.
+
+    socket.emit('change color', 'red');
+    // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
+  }
+}
 
