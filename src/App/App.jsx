0a5afd9d0dc69eece991f6d2cce8eff07749af62
Chernyshov <xpom55@mail.ru>
2018-05-29 13:35:41 +0300

    send event to back and get data for background color via socket.io!

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,14 +8,15 @@ export default class App extends Component {
     this.onClick = this.onClick.bind(this);
 
     this.state = {
-      endpoint: "http://localhost:4001"
+      endpoint: "http://localhost:4001",
+      color: 'black'
     };
   }
   render() {
     return (
       <div>
-        <h1>Hello App HELLO NIKITA уууууууу</h1>
-        <button onClick={this.onClick}>Click me!</button>
+        <h1 style={{ background: this.state.color }}>Hello App HELLO NIKITA уууууууу</h1>
+        <button onClick={this.onClick}>Click to change color from black to RED!</button>
       </div>
     );
   }
@@ -23,12 +24,19 @@ export default class App extends Component {
   onClick() {
     console.log('click');
 
-    const socket = socketIOClient(this.state.endpoint)
+    const socket = socketIOClient(this.state.endpoint);
 
     // this emits an event to the socket (your server) with an argument of 'red'
     // you can make the argument any color you would like, or any kind of data you want to send.
 
     socket.emit('change color', 'red');
+    socket.emit('ferret', // тут указываем id евента
+      'tobi', // первый параметр, который отошлем на сервер
+      (data) => { // второй параметр, функция callback, который передадим на бэк и вызовем
+        console.log(`from server data: ${data}`); // data will be 'woot'
+        this.setState({ color: data });
+      }
+    );
     // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
   }
 }
