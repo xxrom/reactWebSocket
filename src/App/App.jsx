262ff44058456a4e5b6487adee93b993e24507f7
Chernyshov <xpom55@mail.ru>
2018-05-29 15:50:48 +0300

    init user in constructor and then stor data on server

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -11,28 +11,29 @@ export default class App extends Component {
       endpoint: 'http://localhost:4001',
       color: 'black'
     };
+
+    this.socket = socketIOClient(this.state.endpoint);
   }
   render() {
     return (
-      <div >
+      <div>
         <h1 style={{ background: this.state.color }}>Hello App HELLO NIKITA уууууууу</h1>
         <button onClick={this.onClick}>Click to change color from black to RED!</button>
-      </div >
+      </div>
     );
   }
 
   onClick() {
     console.log('click');
 
-    const socket = socketIOClient(this.state.endpoint);
-
     // this emits an event to the socket (your server) with an argument of 'red'
     // you can make the argument any color you would like, or any kind of data you want to send.
 
-    socket.emit(
+    this.socket.emit(
       'toggle color', // тут указываем id евента
       'tobi', // первый параметр, который отошлем на сервер
-      (data) => { // второй параметр, функция callback, который передадим на бэк и вызовем
+      (data) => {
+        // второй параметр, функция callback, который передадим на бэк и вызовем
         console.log(`from server data: ${data}`); // data will be 'woot'
         this.setState({ color: data });
       }
@@ -40,4 +41,3 @@ export default class App extends Component {
     // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
   }
 }
-
