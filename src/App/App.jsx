e6fdf64c28f581fecab1344682ca604c94b0f329
Chernyshov <xpom55@mail.ru>
2018-06-05 17:31:22 +0300

    correct view youtube, send time and events on back koa

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -20,121 +20,63 @@ export default class App extends Component {
     this.socket.on('toggle color auto', (col) => { // подписываемся на рассылку с бэка
       console.log('toggle color auto');
       this.setState({
-        color: col,
-        playVideo: !this.state.playVideo
+        color: col
       });
     });
 
-    this.socket.on('onPauseYouTube', () => {
-      console.log('onPauseYouTube');
-      // this.onPauseYouTube();
-    });
-    this.socket.on('onPlayYouTube', () => {
-      console.log('onPlayYouTube');
-      // this.onPlayYouTube();
+    this.socket.on('youtubeStart', () => {
+      console.log('youtubeStart');
+
+      this.player
+        .playVideo();
     });
 
-    // const opts = {
-    //   height: '390',
-    //   width: '640',
-    //   playerVars: { // https://developers.google.com/youtube/player_parameters
-    //     autoplay: 1,
-    //     start: 10,
-    //     end: 20
-    //   }
-    // };
+    this.socket.on('youtubePause', () => {
+      console.log('youtubePause');
 
-    // this.player = (<YouTube
-    //   videoId="2g811Eo7K8U"
-    //   opts={opts}
-    //   onReady={this.onReadyYouTube}
-    //   onPlay={this.onPlayYouTube}
-    //   onPause={this.onPauseYouTube}
-    // />);
+      this.player
+        .pauseVideo();
+    });
 
-    // const div = document.createElement('div');
-    // div.id = 'player-1';
-    // document.body.appendChild(div);
-    // this.player = YouTubePlayer('player-1', {
-    //   videoId: 'M7lc1UVf-VE'
-    // });
-    // this.player
-    // // Play video is a Promise.
-    // // 'playVideo' is queued and will execute as soon as player is ready.
-    //   .playVideo()
-    //   .then(() => {
-    //     console.log('Starting to play player1. It will take some time to buffer video before it starts playing.');
-    //   });
-
-    // this.getYouTubeVideo = this.getYouTubeVideo.bind(this);
-    // this.onReadyYouTube = this.onReadyYouTube.bind(this);
-    // this.onPlayYouTube = this.onPlayYouTube.bind(this);
-    // this.onPauseYouTube = this.onPauseYouTube.bind(this);
     this.onClick = this.onClick.bind(this);
   }
 
   componentDidMount() {
-    this.player = YouTubePlayer('player-1', {
-      videoId: 'M7lc1UVf-VE'
-    });
-    this.player
-    // Play video is a Promise.
-    // 'playVideo' is queued and will execute as soon as player is ready.
-      .playVideo()
-      .then(() => {
-        console.log('Starting to play player1. It will take some time to buffer video before it starts playing.');
-      });
+    this.setPlayer();
   }
 
   render() {
+    console.log('render');
     return (
       <div>
         <div id="player-1" />
         <h1 style={{ background: this.state.color }}>Hello App HELLO NIKITA уууууууу</h1>
         <button onClick={this.onClick}>Click to change color from black to RED!</button>
-        {this.getYouTubeVideo(this.state)}
       </div>
     );
   }
 
+  setPlayer() {
+    this.player = YouTubePlayer('player-1', {
+      videoId: 'M7lc1UVf-VE'
+    });
 
-  getYouTubeVideo({ playVideo = false }) {
-    return this.player;
+    this.player.on('stateChange', (event) => {
+      console.log(event);
+      this.player
+        .getCurrentTime()
+        .then((time) => {
+          console.log('then thime', Math.round(time));
+          this.socket.emit('youtubeEvent', event.data, Math.round(time));
+        });
+    });
   }
-
-  onReadyYouTube(event) {
-    console.log('onReadyYouTube');
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-    // this.player = event.target;
-  }
-
-  // onPlayYouTube() {
-  //   console.log('onPlayYouTube');
-  //   this.socket.emit('onPlayYouTube');
-  // }
-  // onPauseYouTube() {
-  //   console.log('onPauseYouTube');
-  //   this.socket.emit('onPauseYouTube');
-  // }
 
   onClick() {
     console.log('click');
-    this.socket.emit('toggle color auto');
-    this.player.stopVideo();
-
-    // this emits an event to the socket (your server) with an argument of 'red'
-    // you can make the argument any color you would like, or any kind of data you want to send.
-
-    // this.socket.emit(
-    //   'toggle color', // тут указываем id евента
-    //   'tobi', // первый параметр, который отошлем на сервер
-    //   (data) => {
-    //     // второй параметр, функция callback, который передадим на бэк и вызовем
-    //     console.log(`from server data: ${data}`); // data will be 'woot'
-    //     this.setState({ color: data });
-    //   }
-    // );
-    // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
+    this.setState({
+      color: 'white'
+    });
+    // this.socket.emit('toggle color auto');
   }
 }
